06ddfa785e04a7f20b1ef87127f205ae4d54421b
tiago <tiagobelico@icloud.com>
2026-04-20 00:09:33 -0300

    Atividade Prática - JavaScript básico - matrícula: 910748

---
+++ Desktop/MBD/.py
@@ -0,0 +1,52 @@
+import turtle
+import math
+
+screen= turtle.Screen()
+screen.bgcolor("black")
+screen.title("❤️")
+
+t = turtle.Turtle()
+t.hideturtle()
+t.speed(0)
+t.width(2)
+
+def heart(scale, color):
+  t.color(color)
+  t.begin_fill()
+  for i in range(360):
+    x= scale * 16 * math.sin(math.radians(i))**3
+    y= scale *(
+     13* math.cos(math.radians(i))
+     -5* math.cos(2* math.radians(i))
+     -2* math.cos(3 *math.radians(i))
+     -math.cos(4 * math.radians(i))
+    )
+    t.goto(x,y)
+  t.end_fill()
+t.penup()
+t.goto(0, -10)
+t.pendown()
+
+for s in range(18,13, -1):
+  t.penup()
+  t.goto(0,-10)
+  t.pendown()
+  t.width(2)
+  heart(s,"#330000")
+
+t.penup()
+t.goto(0,-10) 
+t.pendown()
+t.width(2)
+heart(14,"#ff1a1a")
+
+t.penup()
+t.goto(0,-100)
+t.color("#ff4d6d")
+t.write(
+   "I miss you Duda ❤️",
+    align="center",
+    font=("Segoe Ui",22,"bold")
+)
+
+turtle.done()
